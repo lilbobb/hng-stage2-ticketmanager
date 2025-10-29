f5c8339dc9d5c0fac53bec083e7ab50655ac53ed
Robert Adoga <ukurowo@gmail.com>
2025-10-29 11:00:58 +0100

    fix typo in Footer.tsx

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer: React.FC = () => {
   return (
     <footer className="bg-black border-t text-[#9CA3AF] py-8 mt-12">
       <div className="max-w-[1440px] mx-auto px-4 sm:px-6 lg:px-8 text-center">
-        <p>&copy; 2025 TicketFlow. All rights reserved.</p>
+        <p>&copy; 2025 Emprex. All rights reserved.</p>
       </div>
     </footer>
   );
